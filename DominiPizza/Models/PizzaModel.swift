639ff738574ecbf67a5c0d020284c048d10180bc
Оксана Попова <oxana12345616@gmail.com>
2022-12-12 11:54:19 +0300

    fix PizzaVC, PizzaModel

---
+++ DominiPizza/Models/PizzaModel.swift
@@ -0,0 +1,16 @@
+//
+//  PizzaModel.swift
+//  DominiPizza
+//
+//  Created by Оксана Попова on 12.12.2022.
+//
+
+import Foundation
+
+struct Pizza {
+    let titlePizza: String
+    let compoundPizza: String
+    let priсe: Int
+    
+
+    }
